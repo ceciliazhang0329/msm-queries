bf47fe3ab38fa6315692435199004f5a699fce19
ceciliazhang0329 <128743117+ceciliazhang0329@users.noreply.github.com>
2023-11-04 05:21:44 +0000

    aaaa

---
+++ app/controllers/direct_controller.rb
@@ -0,0 +1,2 @@
+class DirectController < ApplicationController
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors", { :controller => "direct", :action => "direct_list" })
+  get("/directors/:director_id", { :controller => "direct", :action => "direct_detail" })
+  get("/directors/youngest", { :controller => "direct", :action => "direct_filter_y" })
+  get("/directors/eldest", { :controller => "direct", :action => "direct_filter_o" })
+  
+  get("/movies", { :controller => "movies", :action => "movie_list" })
+  get("/movies/:movie_id", { :controller => "movies", :action => "movie_detail" })
+
+  get("/actors", { :controller => "actors", :action => "actor_list" })
+  get("/actors/:actor_id", { :controller => "actors", :action => "actor_detail" })
 end
